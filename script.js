3813e9fa93bde62b77b747914cd6c790eeead008
Sudhansh702 <snonu522@gmail.com>
2024-03-25 19:19:08 +0530

    calculator final one

---
+++ script.js
@@ -0,0 +1,41 @@
+let a = 0;
+let o = null;
+let b = 0;
+let r = 0;
+var result = document.querySelector("#result")
+function cal(inp){
+    if(inp==='='){
+        if(typeof b == 'number' && typeof a == 'number' && typeof o == 'string'){
+            result.innerHTML = calulate(a,o,b)
+            a=null 
+            b=null
+            o=null
+        }
+    }
+    else if (typeof inp=="number") {
+        if(typeof o == 'string'){
+            b= b*10 +inp
+            result.innerHTML = a + o + b
+        }else{
+            a = a*10 + inp
+            result.innerHTML = a
+        }
+        
+    }
+    else if(typeof inp == "string" ){
+        o = inp
+        result.innerHTML = a + o
+
+    }
+}
+function calulate(a,o,b){
+    if(o=='+'){
+        return a+b
+    }else if(o=='-'){
+        return a-b
+    }else if(o=='*'){
+        return a*b
+    }else if(o=='/'){
+        return a/b
+    }
+}
